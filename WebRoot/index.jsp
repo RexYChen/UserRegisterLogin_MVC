3b80fd6047864b99d89e610facafc6a5949f3f6b
Rex <rexchen.nz@outlook.com>
2017-04-07 00:33:21 +1200

    Add sign out function

--- WebRoot/index.jsp
+++ WebRoot/index.jsp
@@ -1,15 +1,26 @@
 <%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
-
+<%@ page isELIgnored ="false" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
   <head>
     <title>My JSP 'index.jsp' starting page</title>
   </head>
   
-  <body>
+  <body style="text-align:center;">
   
+  	<h2>Welcome xxxxxx home</h2>
+  	<br/>
+  	<div style="text-align:right;">
+  	<c:if test="${user!=null}">
+  		Welcome: ${user.username}
+  		<a href="${pageContext.request.contextPath}/servlet/SignoutServlet">SIGN OUT</a>
+  	</c:if>
   	
-    <a href="${pageContext.request.contextPath}/servlet/RegisterUIServlet">SIGN UP</a>
-    <a href="${pageContext.request.contextPath}/servlet/LoginUIServlet">SIGN IN</a>
+  	<c:if test="${user == null}">
+  		<a href="${pageContext.request.contextPath}/servlet/RegisterUIServlet">SIGN UP</a>
+    	<a href="${pageContext.request.contextPath}/servlet/LoginUIServlet">SIGN IN</a>
+  	</c:if>
+    </div>
   </body>
 </html>
---
+++ src/com/rex/web/controller/SignoutServlet.java
@@ -0,0 +1,56 @@
+package com.rex.web.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.rex.domain.User;
+
+public class SignoutServlet extends HttpServlet {
+
+	/**
+		 * The doGet method of the servlet. <br>
+		 *
+		 * This method is called when a form has its tag value method equals to get.
+		 * 
+		 * @param request the request send by the client to the server
+		 * @param response the response send by the server to the client
+		 * @throws ServletException if an error occurred
+		 * @throws IOException if an error occurred
+		 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		User user = (User) session.getAttribute("user");
+		if(user != null ){
+			session.removeAttribute("user");
+		}
+		
+		//if sign out success. notice user and display message, after 3seconds return to index.jsp
+		request.setAttribute("message", "User Sign out success,After 3 seconds go to home page <meta http-equiv ='refresh' content='3;url="+request.getContextPath()+"/index.jsp'>");
+		request.getRequestDispatcher("/message.jsp").forward(request, response);
+		
+		
+	}
+
+	/**
+		 * The doPost method of the servlet. <br>
+		 *
+		 * This method is called when a form has its tag value method equals to post.
+		 * 
+		 * @param request the request send by the client to the server
+		 * @param response the response send by the server to the client
+		 * @throws ServletException if an error occurred
+		 * @throws IOException if an error occurred
+		 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
